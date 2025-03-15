4951ac94da7074ef8c364a75d08d4c8904a4320f
Hepsox <marie.delaire@outlook.com>
2025-03-15 14:43:29 +0100

    FAQ rework

--- src/app/list-questions/list-questions.component.ts
+++ src/app/list-questions/list-questions.component.ts
@@ -10,19 +10,13 @@ import { QuestionsService } from '../questions.service';
 })
 export class ListQuestionsComponent {
   listeQuestions$: Observable<Question[]> = this.service.getAllQuestions();
-  questionsOuvertes: boolean[] = []; // Tableau pour suivre les réponses ouvertes
+
+  questionOuverte: number | null = null;
 
   constructor(private service: QuestionsService) {}
 
   ngOnInit(): void {
     // Initialisation de toutes les réponses à "false" au début
-    this.listeQuestions$.subscribe((questions) => {
-      this.questionsOuvertes = new Array(questions.length).fill(false);
-    });
-  }
-
-  toggleReponse(index: number): void {
-    // Au clic, on change l'état de la réponse (false <=> true)
-    this.questionsOuvertes[index] = !this.questionsOuvertes[index];
+    this.listeQuestions$.subscribe((questions) => {});
   }
 }
