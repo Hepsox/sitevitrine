f0def6ae70073b83ca936b0ef8ff453797f0335c
Hepsox <marie.delaire@outlook.com>
2025-03-16 14:53:37 +0100

    Slider logos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { After } from 'v8';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('logosSlide', { static: false }) logosSlide!: ElementRef;
+
+  ngAfterViewInit() {
+    if (this.logosSlide) {
+      this.cloneLogosSlide();
+    } else {
+      console.error('logosSlide is undefined');
+    }
+  }
+
+  cloneLogosSlide() {
+    const copy = this.logosSlide.nativeElement.cloneNode(true);
+    const logosContainer = document.querySelector('.logos');
+    logosContainer?.appendChild(copy);
+  }
+}
